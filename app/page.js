da5fcd871b66fe50caa6979630fee02381f1b40b
protoserg <srtrenych@gmail.com>
2025-01-07 12:18:59 -0500

    Added image, and made the hero pretty. Also 5xl on header and hero

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import ButtonLogin from "@/components/ButtonLogin";
 import FAQListItem from "@/components/FAQListItem";
+import productDemo from "./productDemo.jpeg";
+import Image from "next/image";
 export default function Home() {
   const isLoggedin = true;
   const name = "Sergio";
@@ -9,7 +11,7 @@ export default function Home() {
       {/* HEADER */}
       <section className="bg-base-200 " id="header">
         {/* Base goes from 100-500 and 100 is usually the background */}
-        <div className="max-w-3xl mx-auto  flex justify-between  items-center px-8 py-2">
+        <div className="max-w-5xl mx-auto  flex justify-between  items-center px-8 py-2">
           <div className="font-bold">SaaS</div>
           <div className="space-x-4 max-md:hidden">
             <a className="link link-hover" href="#pricing">
@@ -27,16 +29,26 @@ export default function Home() {
       </section>
 
       {/* HERO */}
-      <section className=" text-center py-32 px-8 max-w-3xl mx-auto" id="hero">
-        <h1 className="text-4xl lg:text-5xl font-extrabold mb-6  ">
-          Collect customer feedback to build better products
-        </h1>
-        <div className="opacity-90 text-lg">
-          Create a feedback board in minutes, prioritize features, and build
-          products your customers will love.
-        </div>
-        <div className=" mt-8">
-          <ButtonLogin isLoggedin={isLoggedin} name={name} />
+      <section
+        className=" text-center py-32 px-8 max-w-5xl mx-auto flex flex-col lg:flex-row gap-14 items-center lg:items-start lg:text-left"
+        id="hero"
+      >
+        <Image
+          src={productDemo}
+          alt="Product demo"
+          className="w-96 rounded-xl shadow-xl"
+        />
+        <div>
+          <h1 className="text-4xl lg:text-5xl font-extrabold mb-6 ">
+            Collect customer feedback to build better products
+          </h1>
+          <div className="opacity-90 text-lg">
+            Create a feedback board in minutes, prioritize features, and build
+            products your customers will love.
+          </div>
+          <div className=" mt-8">
+            <ButtonLogin isLoggedin={isLoggedin} name={name} />
+          </div>
         </div>
       </section>
 
