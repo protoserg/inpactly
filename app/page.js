b0112f9b858140060de3f3f38d3f27208c6f45f9
protoserg <srtrenych@gmail.com>
2025-01-14 16:00:08 -0500

    Fixed Logout

--- app/page.js
+++ app/page.js
@@ -25,7 +25,6 @@ export default async function Home() {
 
           <div>
             <ButtonLogin session={session} />
-            <ButtonLogout session={session} />
           </div>
         </div>
       </section>
--- components/ButtonLogout.js
+++ components/ButtonLogout.js
@@ -2,7 +2,11 @@
 
 import { signOut } from "next-auth/react";
 const ButtonLogout = () => {
-  return <button className="btn btn-ghost" onClick={() => signOut()}></button>;
+  return (
+    <button className="btn btn-ghost" onClick={() => signOut()}>
+      Logout
+    </button>
+  );
 };
 
 export default ButtonLogout;
